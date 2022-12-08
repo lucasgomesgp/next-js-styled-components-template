87ba5c7c9e543d5696a3b5b190f28d541569e2f6
Lucas Gomes <lucasgomesbt@gmail.com>
2022-12-08 15:12:00 -0300

    Template completed

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../styles/theme";
+import { GlobalStyles } from "../../styles/global";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
---
+++ styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  background-color: red;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
